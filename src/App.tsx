51bf044fa9d9d55634c333a666590427b4736673
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-09-12 00:54:49 +0700

    fix: tailwind doesn't work

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
       <>
         <div className="App">
           <header className="App-header">
-            <h1 className="text-pink-200">React App</h1>
+            <h1 className="font-bold text-yellow-500">React App</h1>
+           <h1 className="text-yellow-500">Test Tailwind</h1> 
           </header>
         </div>
       

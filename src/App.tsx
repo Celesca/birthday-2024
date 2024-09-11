119ce46e58e6f3d53a43766d5291f1ce76d38af1
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-09-12 00:50:02 +0700

    init: project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="text-pink-200">React App</h1>
+          </header>
+        </div>
+      
+      </>
+  )
+}
+
+export default App
